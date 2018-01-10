70a0cc9b739d29b1a183cf047c194aeed4f92819
abhinab <abhinabkanrar@gmail.com>
2018-01-10 15:50:52 +0530

    initial commit

---
+++ src/main/java/com/n26/txnanalysis/TxnAnalysisApplication.java
@@ -0,0 +1,12 @@
+package com.n26.txnanalysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TxnAnalysisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TxnAnalysisApplication.class, args);
+	}
+}
---
+++ src/test/java/com/n26/txnanalysis/TxnAnalysisApplicationTests.java
@@ -0,0 +1,16 @@
+package com.n26.txnanalysis;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TxnAnalysisApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
